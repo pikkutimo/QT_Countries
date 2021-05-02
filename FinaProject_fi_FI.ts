fedac102044a57618442dbd63897a112b2adf735
Timo Vilen <timo.a.vilen@tuni.fi>
2021-05-02 09:34:55 +0300

    Translations

--- FinaProject_fi_FI.ts
+++ FinaProject_fi_FI.ts
@@ -1,3 +1,83 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="FinaProject_fi_FI"></TS>
+<TS version="2.1" language="fi_FI">
+<context>
+    <name>ComparisonDialog</name>
+    <message>
+        <location filename="comparisondialog.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>dialogi</translation>
+    </message>
+    <message>
+        <location filename="comparisondialog.ui" line="31"/>
+        <source>Country to compare</source>
+        <translation>vertailtava maa</translation>
+    </message>
+    <message>
+        <location filename="comparisondialog.ui" line="46"/>
+        <source>Country</source>
+        <translation>valitttu maa</translation>
+    </message>
+    <message>
+        <location filename="comparisondialog.ui" line="56"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Countries of the World</source>
+        <translation>Maailman maat</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="24"/>
+        <source>Compare</source>
+        <translation>Vertaile</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <source>Sort by Name</source>
+        <translation>Lajittele nimen mukaan</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>Sort by Area</source>
+        <translation>Lajittele koon mukaan</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>Sort by Population</source>
+        <translation>Lajittele väkimäärän mukaan</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>selectionDialog</name>
+    <message>
+        <location filename="selectiondialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Valitse</translation>
+    </message>
+    <message>
+        <location filename="selectiondialog.ui" line="33"/>
+        <source>Selected country</source>
+        <translation>Valittu maa</translation>
+    </message>
+    <message>
+        <location filename="selectiondialog.ui" line="40"/>
+        <source>Compare to</source>
+        <translation>Vertailuun</translation>
+    </message>
+    <message>
+        <location filename="selectiondialog.ui" line="50"/>
+        <source>Choose Random</source>
+        <translation>Valitse satunnainen</translation>
+    </message>
+</context>
+</TS>
